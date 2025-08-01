04cf1601e1cf4d630caeda5e933bb4da8ffc5b46
Esprees0 <kornna17kornstonk@gmail.com>
2025-08-01 11:50:21 +0700

    test-1--fail

---
+++ Hw1/Client-test.py
@@ -0,0 +1,57 @@
+from socketio import AsyncClient
+import asyncio
+from json import dumps
+from aioconsole import ainput
+
+if __name__ == '__main__':
+    IpAddress = '127.0.0.1'
+    PORT = '8080'
+    ClientName = input(str('Name you want to show in chat'))
+    roomName = 'Marvel'
+    messageTosend = ''
+
+
+    sio = AsyncClient()
+    FullIp = 'http://'+IpAddress+':'+PORT
+
+    @sio.event
+    async def connect():
+        print('Connected to server')
+        await sio.emit('join_chat', {'room': roomName,'name': ClientName})
+
+    @sio.event
+    async def get_message(message):
+        if ClientName == message['from']:
+            print('You : ' + message['message'])
+        else:
+            print(message['from']+' : '+message['message'])
+
+    async def send_message():
+        while True:
+            await asyncio.sleep(0.01)
+            messageTosend = await ainput()
+            await sio.emit('send_chat_room', {'message': messageTosend,'name': ClientName, 'room': roomName})
+
+            if messageTosend.lower() == '/exit':
+                await sio.emit('exit_chat', {'room': roomName, 'name': ClientName})
+                print("You have left the chat.")
+                await sio.disconnect()
+                break
+
+            await sio.emit('send_chat_room', {
+                'message': messageTosend,
+                'name': ClientName,
+                'room': roomName
+            })
+        
+    async def connectToserver():
+        await sio.connect(FullIp)
+        await sio.wait()
+    
+    async def main(IpAddress):
+        await asyncio.gather(
+            connectToserver(),
+            send_message()
+        )
+
+    asyncio.run(main(FullIp))
--- Hw1/Server_Room.py
+++ Hw1/Server_Room.py
@@ -2,31 +2,92 @@ import asyncio
 from aiohttp import web
 import socketio
 from json import dumps
+from datetime import datetime
 
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
 
+# เก็บข้อมูล client
+clients = {}
+
 @sio.event
 async def join_chat(sid, message):
-        print(message.get('name', sid) + ' joined to {}'.format(message['room']))
-        await sio.enter_room(sid, message['room'])
+    name = message.get('name', sid)
+    room = message['room']
+    join_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    # บันทึกข้อมูล client
+    clients[sid] = {
+        'name': name,
+        'room': room,
+        'join_time': join_time,
+        'leave_time': None
+    }
+
+    print(f"{name} joined to {room} at {join_time}")
+    await sio.enter_room(sid, room)
+
+    await sio.emit('user_joined', {
+        'name': name,
+        'room': room,
+        'join_time': join_time
+    }, room=room)
 
 @sio.event
 async def exit_chat(sid, message):
-        print(message.get('name', sid) + ' exited from {}'.format(message['room']))
-        await sio.leave_room(sid, message['room'])
+    room = message['room']
+    name = message.get('name', sid)
+    leave_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    if sid in clients:
+        clients[sid]['leave_time'] = leave_time
+
+    print(f"{name} exited from {room} at {leave_time}")
+
+    await sio.emit('user_left', {
+        'name': name,
+        'room': room,
+        'leave_time': leave_time
+    }, room=room)
+
+    await sio.leave_room(sid, room)
+
+@sio.event
+async def user_joined(data):
+    print(f"{data['name']} joined {data['room']} at {data['join_time']}")
+
+@sio.event
+async def user_left(data):
+    print(f"{data['name']} left {data['room']} at {data['leave_time']}")
 
 @sio.event
 async def send_chat_room(sid, message):
-        await sio.emit('get_message', {'message': message['message'], 'from': message['name']}, room=message['room'])
+    await sio.emit('get_message', {
+        'message': message['message'],
+        'from': message['name']
+    }, room=message['room'])
 
 @sio.event
 async def connect(sid, environ):
-    await sio.emit('my_respones', {'data': 'Connected', 'count': 0}, room=sid)
+    await sio.emit('my_response', {'data': 'Connected', 'count': 0}, room=sid)
 
 @sio.event
 async def disconnect(sid):
+    # อัปเดต leave_time ถ้ายังไม่ออกห้อง
+    if sid in clients:
+        name = clients[sid]['name']
+        room = clients[sid]['room']
+        leave_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        clients[sid]['leave_time'] = leave_time
+
+        print(f"{name} disconnected from {room} at {leave_time}")
+        await sio.emit('user_left', {
+            'name': name,
+            'room': room,
+            'leave_time': leave_time
+        }, room=room)
+
     print('Client disconnected')
 
 if __name__ == '__main__':
